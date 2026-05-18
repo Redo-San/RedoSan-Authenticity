aecea891fc51f27a135a9abd6cae06bde146cea6
RedoSan <12227211+Redo-San@users.noreply.github.com>
2026-05-18 17:42:25 +0300

    Fix formatting issues in shared.js

--- Style_Web_Page/shared.js
+++ Style_Web_Page/shared.js
@@ -12,7 +12,7 @@ function isDangerousFile(file) {
     if (name.endsWith(BLOCKED_EXTS[i])) return true;
   }
   return false;
-}
+} 
 
 var MAGIC_BYTES = {
   'image/png':       [[0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A]],
