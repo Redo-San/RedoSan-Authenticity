8f721883c02fb13401269d7a0357775ad555488c
Redo-San <redosan@artlover.com>
2026-05-21 01:12:09 +0300

    Fix CodeQL alerts: replace sanitizeHtml regex with tag whitelist

--- Style_Web_Page/i18n.js
+++ Style_Web_Page/i18n.js
@@ -4,11 +4,13 @@ var i18n = { lang: 'en', data: {} };
 var SUPPORTED = ['en', 'ar', 'fr', 'de', 'es', 'zh', 'ja', 'ko'];
 
 function sanitizeHtml(html) {
-  return html
-    .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
-    .replace(/\s+on\w+\s*=\s*"[^"]*"/gi, '')
-    .replace(/\s+on\w+\s*=\s*'[^']*'/gi, '')
-    .replace(/javascript\s*:/gi, 'blocked:');
+  var allowed = /^(h[23]|p|ul|li|a|br|strong|em|b|i|code|pre|blockquote|ol|span|div)$/i;
+  return html.replace(/<[^>]*>/g, function(m) {
+    var name = m.replace(/<\/?([^\s>/]+).*/, '$1');
+    if (!allowed.test(name)) return '';
+    return m.replace(/\s+on\w+\s*=\s*(?:"[^"]*"|'[^']*'|[^\s>]+)/gi, '')
+            .replace(/href\s*=\s*"javascript:/gi, 'href="#"');
+  });
 }
 
 // Browser language fallback mapping
