832fb396741616160633e6604cda8ae4a12c7729
Redo-San <redosan@artlover.com>
2026-05-29 00:09:03 +0300

    rename Identity/ → Decentralized_Identity_DID/ + update references

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@ var JS_WHITELIST = [
   '/RedoSan-Authenticity/Pixel_Injection/watermark_advanced_ui.js',
   '/RedoSan-Authenticity/Pixel_Injection/advanced_watermarking.js',
   '/RedoSan-Authenticity/Timestamp/timestamp.js',
-  '/RedoSan-Authenticity/Identity/did.js',
+  '/RedoSan-Authenticity/Decentralized_Identity_DID/did.js',
   '/RedoSan-Authenticity/Fingerprint/hashing.js',
   '/RedoSan-Authenticity/Fingerprint/fingerprint_ui.js',
   '/RedoSan-Authenticity/C2PA/cbor.js',
