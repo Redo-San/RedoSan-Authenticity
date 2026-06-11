6dd3a3de424065ed8b3bce6f73ec9537229013f8
RedoSan <12227211+Redo-San@users.noreply.github.com>
2026-06-11 21:28:16 +0300

    fix: force SW cache cleanup on activate
    Adds cache cleanup on service worker activate event to force old caches to purge.

--- sw.js
+++ sw.js
@@ -260,7 +260,12 @@ self.addEventListener("install", function () {
 });
 
 self.addEventListener("activate", function (event) {
-  event.waitUntil(clients.claim());
+  event.waitUntil(Promise.all([
+    clients.claim(),
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) { return caches.delete(k); }));
+    })
+  ]));
 });
 
 self.addEventListener("fetch", function (event) {
