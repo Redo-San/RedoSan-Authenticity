ed21b81bc1880ed12b210b2ddf5a56987c060ebe
Redo-San <redosan@artlover.com>
2026-08-07 05:54:03 +0300

    fix(devtools): restore biome check scope to cli sources; silence opencode plugin deps

--- .tools/Developer_Toolkit/workbox-config.js
+++ .tools/Developer_Toolkit/workbox-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   globDirectory: ".",
-  globPatterns: ["Style/**/*.{js,css,yml}", "vendor/**/*.js", "index.html", "404.html", "sw.js"],
+  globPatterns: [
+    "Style/**/*.{js,css,yml}",
+    "vendor/**/*.js",
+    "index.html",
+    "404.html",
+    "sw.js",
+  ],
   globIgnores: ["Style/lang/i18n-data.js", "Style/pages/"],
   swDest: "sw-precache.js",
   inlineWorkboxRuntime: true,
